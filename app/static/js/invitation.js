37001616f18580b817e060b32fc73f4521980018
whrksp126 <whrksp126@naver.com>
2023-04-30 04:29:57 +0900

    test tmap

--- app/static/js/invitation.js
+++ app/static/js/invitation.js
@@ -364,13 +364,9 @@ function kakaoMap() {
 }
 // 네이버 지도 내비
 function naverMap() {
-  // `nmap://route/car?
-  //   slat=37.4640070&
-  //   slng=126.9522394&
-  //   sname=%EC%84%9C%EC%9A%B8%EB%8C%80%ED%95%99%EA%B5%90&
-  //   dlat=37.5209436&
-  //   dlng=127.1230074&
-  //   dname=%EC%98%AC%EB%A6%BC%ED%94%BD%EA%B3%B5%EC%9B%90&
-  //   appname=com.example.myapp`
   window.location.href = `nmap://route/car?dlat=${wedding_schedule_dict.lat}&dlng=${wedding_schedule_dict.lng}&dname=${wedding_schedule_dict.hall_name}&appname=wedding`;
+}
+function tMap(){
+  const url = `tmap://search?name=${wedding_schedule_dict.hall_name}&lon=${wedding_schedule_dict.lat}&lat=${wedding_schedule_dict.lng}`;
+  window.location.href = url;
 }
