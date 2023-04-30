6337032876a90cb581784d85fe2ef2038b07d13c
whrksp126 <whrksp126@naver.com>
2023-05-01 01:32:36 +0900

    edit socket

--- gunicorn.config.socket.py
+++ gunicorn.config.socket.py
@@ -4,7 +4,7 @@ import multiprocessing
 workers = multiprocessing.cpu_count() * 2 + 1
 
 # Nginx server 블록에서 설정한 내부 소켓 경로 사용
-binde = 'unix:/tmp/app.sock'
+bind = 'unix:/tmp/app.sock'
 
 # wsgi_app 지정
 # 명령어로 입력했던 'app:create_app()' 을 설정 파일에서 지정
