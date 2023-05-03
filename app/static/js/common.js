136963cedbdd192188d7033b0d5b9a8c3b6d9b8e
whrksp126 <whrksp126@naver.com>
2023-05-04 01:44:11 +0900

    edit save img py

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -20,6 +20,28 @@ const postApi = (url, data, successFun) => {
   });
 }
 
+// fetch Api를 이용한 Post 요청 함수
+const postApiHasHeader = (url, data, successFun) => {
+  console.log(url)
+  fetch(url, {
+    method: 'POST', // 요청 메서드
+    headers: {
+      'Content-Type': 'application/json' // 요청 헤더 설정
+    },
+    body: data // 요청 바디에 보낼 데이터
+  })
+  .then(response => response.json()) // 응답 데이터를 JSON으로 파싱
+  .then(result => {
+    // 성공적으로 응답 받았을 때 실행할 코드 작성
+    successFun(result)
+    // console.log(result);
+  })
+.catch(error => {
+    // 요청이 실패했을 때 실행할 코드 작성
+    console.error(error);
+  });
+}
+
 // html 추가
 const insertHTML = (element, dataDict) => {
   const __element = document.querySelectorAll(element);
